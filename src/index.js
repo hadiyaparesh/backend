5ef075fb9b2d7984fe29e1d35042952b42b44127
hadiyaparesh <pareshhadiya689@gmail.com>
2025-01-14 12:17:52 +0530

    connect to mongodb

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app= express();
+
+
+;(async()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB-URI}/${DB_NAME}`)
+       app.on("errror",(error)=>{
+        console.log("ERROR: ",error);
+        throw error
+       })
+       app.listen(process.env.PORT,()=>{
+        console.log(`App  is listing on poert ${process.env.PORT}`);
+       })
+    }
+    catch(error){
+        console.error("ERROR:",error)
+        throw error
+    }
+})()
+    */
